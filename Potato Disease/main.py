2b3cbc7df1f2fb47ee023ebf0df1ccd4b48669ed
soniya-builds <soniyasmiles05@gmail.com>
2026-03-05 18:16:08 +0600

    import required lib

--- Potato Disease/main.py
+++ Potato Disease/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
 app = FastAPI()
 @app.get("/ping")
 async def ping():
